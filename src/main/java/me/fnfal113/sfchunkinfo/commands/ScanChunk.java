8a56ccdfd24c425c034d05dc396b1c906e2d9d49
FN-FAL113 <88238718+FN-FAL113@users.noreply.github.com>
2021-12-27 14:55:52 +0800

    Added ability to scan others chunk (need perm)

--- src/main/java/me/fnfal113/sfchunkinfo/commands/ScanChunk.java
+++ src/main/java/me/fnfal113/sfchunkinfo/commands/ScanChunk.java
@@ -1,27 +1,20 @@
 package me.fnfal113.sfchunkinfo.commands;
 
-import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
-import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
-import io.github.thebusybiscuit.slimefun4.implementation.setup.SlimefunItemSetup;
-import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.protection.Interaction;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Chunk;
-import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
-public class ScanChunk implements CommandExecutor {
+public class ScanChunk implements TabExecutor {
 
     private final Map<String, Integer> AMOUNT = new HashMap<>();
 
@@ -30,13 +23,31 @@ public class ScanChunk implements CommandExecutor {
 
         if (sender instanceof Player){
             Player player = (Player) sender;
-            if(player.hasPermission("sfchunkinfo.scan")) {
-                Chunk chunk = player.getLocation().getChunk();
+            if(args.length == 0) {
+                if (player.hasPermission("sfchunkinfo.scan")) {
+                    Chunk chunk = player.getLocation().getChunk();
 
-                getAmount(chunk, player);
+                    getAmount(chunk, player);
 
+                } else {
+                    player.sendMessage("You don't have permission to use this command (perm mode: sfchunkinfo.scan)");
+                }
             } else {
-                player.sendMessage("You don't have permission to use this command (perm mode: sfchunkinfo.scan)");
+                if (player.hasPermission("sfchunkinfo.scan.others")) {
+                    Player target = Bukkit.getPlayer(args[0]);
+
+                    if(target == null){
+                        player.sendMessage("Player cannot be null or offline");
+                        return true;
+                    }
+
+                    Chunk chunk = target.getLocation().getChunk();
+
+                    getAmountOthers(chunk, target, player);
+
+                } else {
+                    player.sendMessage("You don't have permission to use this command (perm mode: sfchunkinfo.scan.others)");
+                }
             }
         }
 
@@ -65,7 +76,7 @@ public class ScanChunk implements CommandExecutor {
             }
         }
 
-        player.sendMessage(ChatColor.GOLD + "# of Slimefun items on this chunk:");
+        player.sendMessage(ChatColor.GOLD + "# of Slimefun items on this chunk:", "");
 
         if (AMOUNT.isEmpty()) {
             player.sendMessage(ChatColor.YELLOW + "No Slimefun items on this chunk");
@@ -80,4 +91,48 @@ public class ScanChunk implements CommandExecutor {
 
     }
 
+    public void getAmountOthers(Chunk chunk, Player player, Player sender){
+        for(int y = 0; y <= 319; y++) {
+            for(int x = 0; x <= 15; x++) {
+                for(int z = 0; z <= 15; z++) {
+                    Block itemStack = chunk.getBlock(x, y, z);
+                    if(BlockStorage.hasBlockInfo(itemStack)) {
+                        AMOUNT.put(Objects.requireNonNull(BlockStorage.check(itemStack)).getItemName(),  AMOUNT.getOrDefault(Objects.requireNonNull(BlockStorage.check(itemStack)).getItemName(), 0) + 1);
+                    }
+                }
+            }
+        }
+
+        sender.sendMessage(ChatColor.GOLD + "# of Slimefun items on " + ChatColor.WHITE + player.getName() + ChatColor.GOLD + " chunk:", "");
+
+        if (AMOUNT.isEmpty()) {
+            sender.sendMessage(ChatColor.YELLOW + "No Slimefun items on " + ChatColor.WHITE + player.getName() + ChatColor.GOLD + " chunk");
+            return;
+        }
+
+        for (Map.Entry<String, Integer> entry : AMOUNT.entrySet()) {
+            sender.sendMessage(entry.getKey() + ": " + ChatColor.GREEN + entry.getValue());
+        }
+
+        AMOUNT.clear();
+
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+
+        if(args.length == 1){
+            List<String> playerNames = new ArrayList<>();
+            Player[] players = new Player[Bukkit.getServer().getOnlinePlayers().size()];
+            Bukkit.getServer().getOnlinePlayers().toArray(players);
+            for (Player player : players) {
+                playerNames.add(player.getName());
+            }
+
+            return playerNames;
+        }
+
+
+        return null;
+    }
 }
